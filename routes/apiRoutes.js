1c7434147019924d809e4407c0bbc082e91d6b06
NelsonCad <NelsonCad97@gmail.com>
2019-08-21 16:57:56 -0700

    code for both routes are finished

---
+++ routes/apiRoutes.js
@@ -0,0 +1,12 @@
+let friendsData = require("./Data/friends");
+
+module.exports = function(app) {
+
+    app.get("api/friends", function(req, res) {
+        res.json(friendsData);
+    });
+
+    app.post("/api/friends", function (req, res) {
+        friendsData.push(req.body);
+    });
+};
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,12 @@
 const path = require("path");
 
+module.exports = function(app) {
 
-app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "questions"));
-});
-
-app.get("*", function (req,res) {
-    res.sendFile(path.join(__dirname, "index"))
-})
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../questions.html"));
+    });
+    
+    app.get("*", function (req,res) {
+        res.sendFile(path.join(__dirname, "../index.html"))
+    });
+};
