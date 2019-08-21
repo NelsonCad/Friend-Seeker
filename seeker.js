f8f7d45dabd0bd7aaa84c6d23c43fc0fba85cfa0
NelsonCad <NelsonCad97@gmail.com>
2019-08-21 14:03:25 -0700

    added dependencies

--- seeker.js
+++ seeker.js
@@ -6,8 +6,17 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+
+
+
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
