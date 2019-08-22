ba075418211c06cce6ec9aefbea342538df906e5
NelsonCad <NelsonCad97@gmail.com>
2019-08-21 17:30:13 -0700

    added some friends to be obtained

---
+++ routes/Data/friends.js
@@ -0,0 +1,26 @@
+let friends = [
+    {
+        name: "Dave Strider",
+        scores: [
+            3,4,4,4,5,3,2,3,4,3
+        ]
+    },
+    {
+        name: "John Egbert",
+        scores: [
+            3,4,2,2,3,1,3,3,5,5
+        ]
+    },
+    {
+        name: "Jade Harley",
+        scores: [
+            4,2,4,3,4,2,4,2,4,4
+        ]
+    },
+    {
+        name: "Rose Lalonde",
+        scores: [
+            5,4,4,5,5,4,4,4,3,3,
+        ]
+    }
+]
