e97f37748a564789a5dbc276422b35498be14b41
NelsonCad <NelsonCad97@gmail.com>
2019-08-22 19:10:46 -0700

    pushes new data into friends array, responds with nobody at all times though

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,29 +8,31 @@ module.exports = function (app) {
 
     app.post("/api/friends", function (req, res) {
 
-        let friends = friendsData.friends;
-        let newBestFriend;
+        let comparison = { name: "nobody", similarity: 40 };
 
-        let comparison = { name: "nobody", similarity: 40 }
-
-        for (f = 0; f < friends.length; f++) {
+        for (f = 0; f < friendsData.friends.length; f++) {
 
             let likability = 0;
 
-            for (i = 0; i < friends.score.length; i++) {
-                let similarity = friends.score[i] - req.body.score[i];
+            for (i = 0; i < friendsData.friends[f].scores.length; i++) {
+                let friendscore = parseInt(friendsData.friends[f].scores[i]);
+                let userscore = parseInt(req.body.scores[i]);
+
+                let similarity = friendscore - userscore;
 
                 likability += similarity;
             };
 
             if (likability < comparison.likability) {
-                newBestFriend = friends[f];
-                comparison = {name: friends[f].name, similarity: likability}
-            }
+                newBestFriend = friendsData.friends[f];
+                comparison = {name: friendsData.friends[f].name, similarity: likability};
+            };
+            
         };
 
-        res.json(newBestFriend)
-
-        friends.push(req.body);
+        friendsData.friends.push(req.body);
+        res.json(comparison.name);
     });
+
+
 };
