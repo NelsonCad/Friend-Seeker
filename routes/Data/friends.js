20f421ce549c5da200811f357b5c5a5a5d859784
NelsonCad <NelsonCad97@gmail.com>
2019-08-22 19:57:00 -0700

    added photos for the base friends

--- routes/Data/friends.js
+++ routes/Data/friends.js
@@ -1,24 +1,28 @@
 let friends = [
     {
         name: "Dave Strider",
+        photo: "photos/Daev.PNG",
         scores: [
             "3", "4", "4", "4", "5", "3", "2", "3", "4", "3"
         ]
     },
     {
         name: "John Egbert",
+        photo: "photos/Jhon.PNG",
         scores: [
             "3", "4", "2", "2", "3", "1", "3", "3", "5", "5"
         ]
     },
     {
         name: "Jade Harley",
+        photo: "photos/Jaed.PNG",
         scores: [
             "4", "2", "4", "3", "4", "2", "4", "2", "4", "4"
         ]
     },
     {
         name: "Rose Lalonde",
+        photo: "photos/Rozz.PNG",
         scores: [
             "5", "4", "4", "5", "5", "4", "4", "4", "3", "3",
         ]
